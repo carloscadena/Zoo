89ce4a859d5e2759ed8bbad2c979c2215f92b141
carlos cadena <carloscadena@live.com>
2018-10-17 00:08:57 -0700

    beagle class

--- Zoo/Classes/Beagle.cs
+++ Zoo/Classes/Beagle.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    class Beagle
+    class Beagle : Dog
     {
+        public string isHunter { get; set; } = "I was bred to hunt";
+        public override string EatFood()
+        {
+            return "mmhmm more kibble please";
+        }
+
+        public override string FormsGroup()
+        {
+            return "I yearn to be a part of a pack";
+        }
+
+        public override string MakesBabies()
+        {
+            return "As often as I can";
+        }
+        public override string LearnTrick()
+        {
+            return "I can roll over!";
+        }
     }
 }
