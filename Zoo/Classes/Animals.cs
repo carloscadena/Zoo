46caec995ca9fce918316c5798ad68beef7968c0
carlos cadena <carloscadena@live.com>
2018-10-16 22:56:57 -0700

    Animals class

--- Zoo/Classes/Animals.cs
+++ Zoo/Classes/Animals.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    class Animals
+    public abstract class Animals
     {
+        public abstract string HasHeart { get; set; }
+        public virtual string BreathesAir { get; set; }
+
+        public abstract void MakeBabies();
+        public abstract string EatFood();
+        public virtual string MakeSound()
+        {
+            return "Moo";
+        }
     }
 }
